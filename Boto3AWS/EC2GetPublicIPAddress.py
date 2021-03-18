a21c659d7fabd9360cb1e9ed07d8d144499634c3
ashlinrajangabriel <71674232+ashlinrajangabriel@users.noreply.github.com>
2021-03-18 15:00:43 +0530

    Check for Public IP addresses

---
+++ Boto3AWS/EC2GetPublicIPAddress.py
@@ -0,0 +1,21 @@
+import boto3
+# Check for Running EC2 Instance
+def get_running_instances():
+    ec2_client = boto3.client("ec2", region_name="eu-west-1")
+    reservations = ec2_client.describe_instances(Filters=[
+        {
+            "Name": "instance-state-name",
+            "Values": ["running"],
+        }
+    ]).get("Reservations")
+
+    for reservation in reservations:
+        for instance in reservation["Instances"]:
+            instance_id = instance["InstanceId"]
+            instance_type = instance["InstanceType"]
+            public_ip = instance["PublicIpAddress"]
+            private_ip = instance["PrivateIpAddress"]
+            print(f"{instance_id}, {instance_type}, {public_ip}, {private_ip}")
+
+
+get_running_instances()
